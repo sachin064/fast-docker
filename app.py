8c690cda894b1e43eccd445e1599223254ce3068
Sachin B G <Sachin.B@visionetsystems.com>
2023-10-05 15:35:20 +0530

    changes done

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
-import os
-
-from flask import Flask, render_template, abort, url_for, json, jsonify
-
 import json
+
+from flask import Flask, render_template
 
 app = Flask(__name__,template_folder='.')
 
-with open('file.json', 'r') as myfile:
+with open('file.json', 'r', encoding="utf-8") as myfile:
     data = myfile.read()
 
 @app.route("/")
 def index():
+    """ it returns the """
     return render_template('index.html', title="page", jsonfile=json.dumps(data))
 
 if __name__ == '__main__':
-    
+
     app.run(debug=True, host='0.0.0.0')
